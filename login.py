087b1350e56dae0ffe7a8c2b5339812728c0e47b
Martin Martinez <investingmtz@gmail.com>
2021-01-20 11:55:15 -0600

    Funcion cerrar sesion creada

--- login.py
+++ login.py
@@ -11,15 +11,17 @@ si lo deseas podrás borrar tu usuario y contraseña.
 Estas estarán en un lugar seguro, no te preocupes
 
     1 - Iniciar sesión
-    2 - Crear cuenta
-    3 - Eliminar cuenta
-    4 - Salir
+    2 - Cerrar sesión
+    3 - Crear cuenta
+    4 - Eliminar cuenta
+    5 - Salir
 """
 
 usuario = ''
 contrasenia = ''
+sesion_iniciada = False
 
-#TODO iniciar sesion
+
 def inicio_sesion():
     usuario_file = open('D:\\vs_programs\\automatizar\\login\\login_info.txt', 'r')
     info_texto = usuario_file.read()
@@ -29,9 +31,22 @@ def inicio_sesion():
     contrasenia = input('Ingresa tu contraseña: ')
     if usuario in info_texto and contrasenia in info_texto:
         print('=====Acceso concedido=====')
+        sesion_iniciada = True
+        main()
     else:
         print('Tu usuario o contraseña son incorrectas, vuelve a intentarlo')
         inicio_sesion()
+
+#TODO cerrar sesion
+def cerrar_sesion():
+    sesion_iniciada = sesion_iniciada
+    if sesion_iniciada == True:
+        print('=====Sesion cerrada=====')
+        sesion_iniciada = False
+        main()
+    else:
+        print('Primero debes iniciar sesion!')
+        main()
 #TODO crear cuenta
 
 #TODO eliminar cuenta
@@ -41,19 +56,20 @@ def inicio_sesion():
 def main():
     print(menu)
     eleccion = ''
-    while eleccion != 4:
+    while eleccion != 5:
         eleccion = input('Ingrese la opción de su elección: ')
         if eleccion == 1:
-            pass
-            #funcion inicio sesion
+            inicio_sesion()
         elif eleccion == 2:
+            cerrar_sesion()
+        elif eleccion == 3:
             pass
             #funcion crear cuenta
-        elif eleccion == 3:
+        elif eleccion == 4:
             pass
             #funcion eliminar cuenta
         else:
             sys.exit()
     
 if __name__ == "__main__":
-    inicio_sesion()
+    main()
