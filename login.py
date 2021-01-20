1495283f00089aaf8ff80858eff7424291345016
Martin Martinez <investingmtz@gmail.com>
2021-01-20 11:25:36 -0600

    Creando login para usar archivo de texto

---
+++ login.py
@@ -0,0 +1,23 @@
+#python3
+#Pequeño programa de inicio de sesión, la información será guardada en un documento de texto
+
+menu = """
+Bienvenido a mi sistema de Login, en el podrás
+crear un usuario y contraseña, iniciar sesion y
+si lo deseas podrás borrar tu usuario y contraseña.
+
+Estas estarán en un lugar seguro, no te preocupes
+
+    1 - Iniciar sesión
+    2 - Crear cuenta
+    3 - Eliminar cuenta
+    4 - Salir
+"""
+
+#TODO iniciar sesion
+
+#TODO crear cuenta
+
+#TODO eliminar cuenta
+
+#TODO salir
